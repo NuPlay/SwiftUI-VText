d8224fda4ddc5d85012b014bb588c2c623e39933
LeeUngjae <rhakrhaehfdl@gmail.com>
2021-04-17 21:48:40 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "VText",
     platforms: [
-           .macOS(.v11), .iOS(.v13), .tvOS(.v13)
+           .macOS(.v11), .iOS(.v14), .tvOS(.v13)
        ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
